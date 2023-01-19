cd7cd6351ca0d00497221ef55e9b934d3d0af52b
PVRPython <pablorychtyk@outlook.de>
2023-01-19 15:55:52 +0100

    thursday updatthursday updatee

--- GeoData/Test.py
+++ GeoData/Test.py
@@ -4,4 +4,8 @@ import geopandas as gpd
 import atlite as at
 
 path=gpd.read_file('gadm_410-levels-ADM_1-JPN.gpkg')
-print(path.geometry)
+#print(path.NAME_1)
+Level1=gpd.read_file("gadmJPN-Level1Areas.json")
+path['ISO']=Level1['ISO_1']
+#print(path.ISO)
+print(path['ISO'=='NA'])
