831de97eb50a60e28a995bc94d79e0d7c2e0107f
Karim Shawky <karim@pop-os.localdomain>
2023-01-20 12:28:42 +0100

    Added Beginning of Analyses of current Japan

---
+++ GeoData/Current_JPN.py
@@ -0,0 +1,38 @@
+
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+csv='global_power_plant_database.csv'
+globalpowerplants= pd.read_csv(csv)
+#globalpowerplants= globalpowerplants.dropna(subset='generation_data_source')
+#globalpowerplants= globalpowerplants.loc[globalpowerplants['country']!= 'USA']
+#globalpowerplants= globalpowerplants.loc[globalpowerplants['country']!= 'AUS']
+#print(globalpowerplants['country_long'].unique())
+
+japan_power=globalpowerplants.loc[globalpowerplants['country']== 'JPN']
+japan_power=japan_power.drop(columns='country')
+japan_power=japan_power.drop(columns='country_long')
+
+japan_power=japan_power.reset_index()
+
+
+jfuel= pd.DataFrame(japan_power.groupby('primary_fuel')['capacity_mw'].sum())
+
+
+jfuel['capacity_mw'].plot.pie(autopct='%1.1f', figsize=(7,7))
+
+japan_power['capacity_factor'] = (japan_power.estimated_generation_gwh_2017 / japan_power.capacity_mw.div(1e3).mul(8760))
+
+jfuel['average_capacity_factor']=japan_power.groupby('primary_fuel')['capacity_factor'].mean()
+
+
+
+
+
+
+
+
+
+
