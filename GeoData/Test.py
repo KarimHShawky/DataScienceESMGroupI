ce85e09132f564f6851421ad2d9e95b70cca4dc0
Karim Shawky <94178518+KarimHShawky@users.noreply.github.com>
2023-01-20 09:08:47 +0100

    Update Test.py
    Added Int. extraction

--- GeoData/Test.py
+++ GeoData/Test.py
@@ -6,6 +6,12 @@ import atlite as at
 path=gpd.read_file('gadm_410-levels-ADM_1-JPN.gpkg')
 #print(path.NAME_1)
 Level1=gpd.read_file("gadmJPN-Level1Areas.json")
-path['ISO']=Level1['ISO_1']
+path['ISO_1']=[]
+path.merge(Level1, on = 'ISO_1')
+
+
+path['ISO_1']= path['ISO_1'].str.extract('(\d+)')
+#data = data.str.extract('(\d+)')
+
 #print(path.ISO)
 print(path['ISO'=='NA'])
